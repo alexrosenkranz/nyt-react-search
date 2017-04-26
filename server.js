808329fad325bcc9c7fa1ece33040d8ee1650a24
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2017-04-26 11:37:38 -0400

    updated readme

--- server.js
+++ server.js
@@ -1,56 +1,61 @@
 // Dependencies
 var express = require('express')
-var mongojs = require('mongojs')
+var mongoose = require('mongoose')
 var logger = require('morgan')
 var bodyParser = require('body-parser')
 var request = require('request')
 
-var PORT = process.env.PORT || 3001;
-var app = express();
+mongoose.Promise = Promise
+
+var PORT = process.env.PORT || 3001
+var app = express()
 
 // Set the app up with morgan
-app.use(logger('dev'));
+app.use(logger('dev'))
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const apiController = require('./api/controllers/apiController.js')
+
 
 // Database configuration
-var databaseUrl = process.env.MONGODB_URI || 'nytreact_db';
-var collections = ['articles'];
+var databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost/nytreact_db';
 
-// Hook mongojs config to db variable
-var db = mongojs(databaseUrl , collections);
+// Database configuration with mongoose
+mongoose.connect(databaseUrl)
+const db = mongoose.connection
 
-// Log any mongojs errors to console
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Show any mongoose errors
+db.on('error', function (error) {
+  console.log('Mongoose Error: ', error)
+})
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.')
+})
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist'));
 }
 
-/*
-  if we don't do this here then we'll get this error in apps that use this api
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
 
-  Fetch API cannot load No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin is therefore not allowed access. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+app.use('/api', apiController)
 
-  read up on CORs here: https://www.maxcdn.com/one/visual-glossary/cors/
-*/
-  //allow the api to be accessed by other apps
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-    next();
-  });
-
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/public/index.html'));
-  });
+app.get('*', function(req, res) {
+  let filePath = path.join(__dirname, './client/dist/index.html')
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+  console.log("path " + filePath)
+});
 
 // Listen on port 3001
-  app.listen(PORT, function() {
-    console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
-  });
+app.listen(PORT, function() {
+  console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
+});
